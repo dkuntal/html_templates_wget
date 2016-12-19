56c3a2327752b07d06f119d0b9e2e94edcfac25c
dkuntal <dkuntal9@gmail.com>
2016-12-19 13:21:14 +0530

    blog post included in common file

---
+++ webdesign-finder.com/html/political/blog/post_bottom.php
@@ -0,0 +1,38 @@
+							<div class="row blog-buttons columns_padding_5 bottommargin_10">
+								<div class="col-md-6">
+									<a class="vertical-item content-absolute full-content text-center ds inline-block" href="">
+										<div class="item-media">
+											<img src="images/blog-prev.jpg" alt="">
+											<div class="media-links">
+											</div>
+										</div>
+										<div class="item-content">
+											<div class="display_table">
+												<div class="display_table_cell">
+													<p class="vidaloka highlight text-uppercase bottommargin_10">prev</p>
+													<h4 class="bottommargin_0">Consectetur
+														<br> flank drumstick</h4>
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+								<div class="col-md-6">
+									<a class="vertical-item content-absolute full-content text-center ds inline-block" href="">
+										<div class="item-media">
+											<img src="images/blog-next.jpg" alt="">
+											<div class="media-links">
+											</div>
+										</div>
+										<div class="item-content">
+											<div class="display_table">
+												<div class="display_table_cell">
+													<p class="vidaloka highlight text-uppercase bottommargin_10">next</p>
+													<h4 class="bottommargin_0">Cupim flank pork
+														<br> ut pork belly</h4>
+												</div>
+											</div>
+										</div>
+									</a>
+								</div>
+							</div>
---
+++ webdesign-finder.com/html/political/blog/post_menu.php
@@ -0,0 +1,20 @@
+			<section class="page_breadcrumbs ds parallax section_padding_45 column_margin_0">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<h1>Blog Post</h1>
+							<ol class="breadcrumb">
+								<li>
+									<a href="/">
+										Homepage
+									</a>
+								</li>
+								<li>
+									<a href="#">Blog</a>
+								</li>
+								<li class="active">Blog Post</li>
+							</ol>
+						</div>
+					</div>
+				</div>
+			</section>
