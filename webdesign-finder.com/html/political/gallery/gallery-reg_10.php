7d72db19cd7f17362bddf35d90a4f8a786898b58
dkuntal <dkuntal9@gmail.com>
2016-12-22 05:51:10 +0530

    gallery-regular common linked files uploaded

---
+++ webdesign-finder.com/html/political/gallery/gallery-reg_10.php
@@ -0,0 +1,16 @@
+									<div class="vertical-item gallery-item content-absolute text-center ds">
+										<div class="item-media">
+											<img src="images/gallery/10.jpg" alt="">
+											<div class="media-links">
+												<div class="links-wrap">
+													<a class="p-view prettyPhoto " title="" data-gal="prettyPhoto[gal]" href="images/gallery/10.jpg"></a>
+													<a class="p-link" title="" href="gallery-single.html"></a>
+												</div>
+											</div>
+										</div>
+										<div class="item-content">
+											<h5 class="item-meta ">
+												<a href="gallery-single.html">Consetetur sadipscing elitr, sed diam nonumy</a>
+											</h5>
+										</div>
+									</div>
