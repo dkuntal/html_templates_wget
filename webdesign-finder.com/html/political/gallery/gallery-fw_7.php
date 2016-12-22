c5681e9e9f67f91c6ee188a7e55d64b5017d5d99
dkuntal <dkuntal9@gmail.com>
2016-12-22 13:32:28 +0530

    gallery-fw_*.php created for gallery-fullwidth files

---
+++ webdesign-finder.com/html/political/gallery/gallery-fw_7.php
@@ -0,0 +1,19 @@
+									<div class="vertical-item gallery-title-item content-absolute">
+										<div class="item-media">
+											<img src="images/gallery/07.jpg" alt="">
+											<div class="media-links">
+												<div class="links-wrap">
+													<a class="p-view prettyPhoto " title="" data-gal="prettyPhoto[gal]" href="images/gallery/07.jpg"></a>
+													<a class="p-link" title="" href="gallery-single.html"></a>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="item-title text-center">
+										<h3>
+											<a href="gallery-single.html">Dolore magna aliquyam</a>
+										</h3>
+										<span class="categories-links highlight2-links">
+											<a rel="category" href="#" class="theme_button small_button color1">meetings</a>
+										</span>
+									</div>
